18452417a24949987315f32ff071edfe9d97ce97
Alistair A. Israel <aisrael@gmail.com>
2018-03-19 07:55:07 +0800

    crystal init app hi8-test

---
+++ src/hi8-test.cr
@@ -0,0 +1,6 @@
+require "./hi8-test/*"
+
+# TODO: Write documentation for `Hi8::Test`
+module Hi8::Test
+  # TODO: Put your code here
+end
---
+++ src/hi8-test/version.cr
@@ -0,0 +1,3 @@
+module Hi8::Test
+  VERSION = "0.1.0"
+end
